87fc67f5a4336350b97516a0ee72e704e44b7831
Dmitry Vdovin <voidxnull@gmail.com>
2023-05-05 17:16:43 +0400

    Load initial hashes from file

--- indexer-worker/src/backend/near/archive_node.rs
+++ indexer-worker/src/backend/near/archive_node.rs
@@ -13,7 +13,7 @@ pub struct Config {
     pub contract_address: String,
     pub chain_id: String,
     pub rpc_url: String,
-    pub initial_tx_hashes: Vec<String>,
+    pub initial_tx_hashes_path: Option<String>,
 }
 
 pub struct NearArchiveNodeBackend {
@@ -30,56 +30,17 @@ impl Backend for NearArchiveNodeBackend {
     }
 }
 
-async fn get_tx_status(
-    rpc_url: &str,
-    hash: &str,
-    sender: &str,
-) -> Result<FinalExecutionOutcomeView> {
-    let client = reqwest::Client::new();
-    let res: serde_json::Value = client
-        .post(rpc_url)
-        .json(&serde_json::json!({
-            "jsonrpc": "2.0",
-            "id": "dontcare",
-            "method": "tx",
-            "params": [hash, sender]
-        }))
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    let tx = serde_json::from_value::<FinalExecutionOutcomeView>(res["result"].clone())?;
-
-    Ok(tx)
-}
-
-async fn get_block(rpc_url: &str, hash: &str) -> Result<BlockView> {
-    let client = reqwest::Client::new();
-    let res: serde_json::Value = client
-        .post(rpc_url)
-        .json(&serde_json::json!({
-            "jsonrpc": "2.0",
-            "id": "dontcare",
-            "method": "block",
-            "params": {
-                "block_id": hash,
-            }
-        }))
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    let block = serde_json::from_value::<BlockView>(res["result"].clone())?;
-
-    Ok(block)
-}
-
 #[async_trait::async_trait]
 impl BackendMethods for NearArchiveNodeBackend {
     async fn start(self, send: mpsc::Sender<Tx>) -> Result<JoinHandle<Result<()>>> {
-        for tx_hash in self.config.initial_tx_hashes {
+        let hashes_path = self
+            .config
+            .initial_tx_hashes_path
+            .unwrap_or_else(|| "initial_tx_hashes.json".to_owned());
+
+        let hashes: Vec<String> = serde_json::from_str(&std::fs::read_to_string(hashes_path)?)?;
+
+        for tx_hash in hashes {
             let tx = get_tx_status(
                 &self.config.rpc_url,
                 &tx_hash,
@@ -151,4 +112,50 @@ async fn process_tx(
                 .expect("Failed to send tx to the channel");
         }
     }
+}
+
+async fn get_tx_status(
+    rpc_url: &str,
+    hash: &str,
+    sender: &str,
+) -> Result<FinalExecutionOutcomeView> {
+    let client = reqwest::Client::new();
+    let res: serde_json::Value = client
+        .post(rpc_url)
+        .json(&serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "tx",
+            "params": [hash, sender]
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    let tx = serde_json::from_value::<FinalExecutionOutcomeView>(res["result"].clone())?;
+
+    Ok(tx)
+}
+
+async fn get_block(rpc_url: &str, hash: &str) -> Result<BlockView> {
+    let client = reqwest::Client::new();
+    let res: serde_json::Value = client
+        .post(rpc_url)
+        .json(&serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "block",
+            "params": {
+                "block_id": hash,
+            }
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    let block = serde_json::from_value::<BlockView>(res["result"].clone())?;
+
+    Ok(block)
 }
